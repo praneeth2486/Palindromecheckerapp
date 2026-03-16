d16d8cf1acd442e09af939cf31df1a3822f28cfe
praneeth2486 <praneethreddy2486@gmail.com>
2026-03-16 22:12:43 +0530

    UC3:Print a Hardcoded Palindrome Result

--- src/Palindromecheckerapp.java
+++ src/Palindromecheckerapp.java
@@ -1,18 +1,26 @@
+import java.util.Scanner;
+
 public class Palindromecheckerapp {
+
+    static boolean check(String s, int l, int r) {
+        if (l >= r)
+            return true;
+
+        if (s.charAt(l) != s.charAt(r))
+            return false;
+
+        return check(s, l + 1, r - 1);
+    }
+
     public static void main(String[] args) {
-        String input = "madam";
-        boolean isPalindrome = true;
 
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
-        }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
 
-        if (isPalindrome)
-            System.out.println(input + " is a palindrome");
+        if (check(str, 0, str.length() - 1))
+            System.out.println("Palindrome");
         else
-            System.out.println(input + " is not a palindrome");
+            System.out.println("Not Palindrome");
     }
 }
